43ffdf11bd6b80933a4aae501af111043e790956
GFO <gfo146@alumnos.unican.es>
2025-04-08 18:32:16 +0200

    Commit 1

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char username[50];
+    char password[50];
+    char correctUsername[] = "admin";
+    char correctPassword[] = "1234";
+    asdadadadasdassd
+
+    printf("Ingrese su nombre de usuario: ");
+    scanf("%49s", username);
+
+    printf("Ingrese su contraseña: ");
+    scanf("%49s", password);
+
+    if (strcmp(username, correctUsername) == 0 && strcmp(password, correctPassword) == 0) {
+        printf("OK\n");
+    } else {
+        printf("Credenciales incorrectas\n");
+    }
+
+    return 0;
+}
